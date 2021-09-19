9852ba3860ce569e981caef04131c523c6d858be
Thundercat <justin@thundercat.me>
2021-09-19 19:13:29 -0400

    Made corrections to random_color view

--- core/views.py
+++ core/views.py
@@ -5,8 +5,8 @@ from django.utils.crypto import get_random_string
 def random_color(request):
     color_code = get_random_string(6, 'ABCDEF0123456789')
 
-    # For future ability to save color_code user to profile
-    captured_color = str(color_code)
+    # For future ability to save color_code to user profile
+    captured_color = color_code
 
     return render(request, 'core/random_color.html', {
         'color_code': color_code,
